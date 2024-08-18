032557880bc210e1b8e7edd51db7bfa4df90a0f6
Aritra Dey <adey01027@gmail.com>
2024-08-18 23:31:17 +0530

    Added to github

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const navLinks = document.querySelectorAll('nav ul li a');
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    let currentSection = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 60) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(currentSection)) {
+            link.classList.add('active');
+        }
+    });
+});
+
+document.addEventListener("scroll", function() {
+    const backToTopButton = document.querySelector(".back-to-top");
+    if (window.scrollY > 300) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+});
+
+document.querySelector(".back-to-top").addEventListener("click", function(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
